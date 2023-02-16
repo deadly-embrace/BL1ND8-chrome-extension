8ec08b60983918aa2dd5479646008d43bf61e38e
Stephen <stephen.anthony.rivas@gmail.com>
2023-02-16 03:12:10 -0500

    adjusted component distributions

--- src/pages/popup/Popup.tsx
+++ src/pages/popup/Popup.tsx
@@ -1,22 +1,23 @@
 import React from "react";
 import "@pages/popup/Popup.css";
 import logo from "@assets/img/logo.svg";
-// import logo from "@assets/img/logo.svg";
+import SkipButton from "@pages/popup/SkipButton.js";
+
 const Popup = () => {
   return (
     <div className="App">
       <header className="App-header">
         <div className="App-top">
-          <img className="logo" src={logo}></img>
           <div className="username">Munchkin</div>
+          <img className="logo" src={logo}></img>
           <div className="location">NY, NY</div>
         </div>
-        <img
+        {/* <img
           className="App-video"
           src="https://images.wallpaperscraft.com/image/single/cat_kitten_glance_177552_1600x900.jpg"
           alt="logo"
-        />
-        <p>See your match in {30} seconds!</p>
+        /> */}
+        <div>See your match in {30} seconds!</div>
         <button>Skip</button>
         <button>Reveal</button>
       </header>
---
+++ src/pages/popup/SkipButton.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const SkipButton = () => {
+  return (
+    
+  );
+};
+
+export default SkipButton;
